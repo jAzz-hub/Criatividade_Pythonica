fcfa8663d7870fb3180611020d6bd2d2f94d5559
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-09-08 12:06:51 -0300

    Locadora de Ouro Versão 0.5
    Versão Beta de um projeto pessoal, que consiste em armazenar meus filmes favoritos e mostrar suas devidas informações através das requisições feitas ao programa. Estou acumulando ideias para implementar nesse proejto.
    Sujestões e críticas são sempre bem vindas!

---
+++ Locadora Com dicts.py
@@ -0,0 +1,59 @@
+#Criando uma locadora e verificando se o filme que o usuário procura está disponível:
+
+Locadora=[{
+    
+        'Título':'O jogo da imitação',
+        'Ano':'2014',
+        'Diretor':'Mordem Tyldum'
+
+    },{
+        
+        'Título':'Pulp Fiction',
+        'Ano':'1994',
+        'Diretor':'Quentin Tarantino'
+    
+    },{
+
+        'Título':'Baby Driver',
+        'Ano':'2017',
+        'Diretor':'Edgar Wright'
+
+    },{
+        
+        'Título':'Forrest Gump-O Contador de Histórias',
+        'Ano':'1994',
+        'Diretor':'Robert Zemeckis'
+    
+    },{
+
+        'Título':'Era uma Vez em... Hollywood',
+        'Ano':'2019',
+        'Diretor':'Quentin Tarantino'
+    
+    },{
+
+        'Título':'O Que Fazemos nas Sombras',
+        'Ano':'2014',
+        'Diretor':'Taika Waititi, Jamaine Clement'
+
+    }]
+
+
+
+print('Os filmes da locadora são')
+
+for i in Locadora:
+    T=i['Título']
+    print(f'[ {Locadora.index(i)} ] ==>  {T} ')
+
+info=str(input('Deseja saber um pouco mais sobre algum filme?'))
+
+if info in ' S s Sim SIm SIM siM sIM Sim sIm ':
+    
+    FilmeEscolhido=int(input('Número do Filme: '))
+    print(f'''      O diretor do filme escolhido é : {Locadora[FilmeEscolhido]['Diretor']}
+    O ano de lançamento é : {Locadora[FilmeEscolhido]['Ano']}''')
+
+else:
+    print('Tudo bem, Adeus! :D')
+    exit()
